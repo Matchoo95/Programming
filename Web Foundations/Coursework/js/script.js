74b09f18a1b8fd1b2b71cdd731cfcbb815cd22f6
Matchoo95 <mattmatth123@googlemail.com>
2016-09-20 13:43:35 +0100

    Add files via upload

---
+++ Web Foundations/Coursework/js/script.js
@@ -0,0 +1,46 @@
+// basic js to update aside bar across all html pages
+
+
+document.getElementById("sidecontents").innerHTML =
+
+'<h1>Recent Worklog Entries</h1>' +
+  '<hr />' +
+  '<ul>' +
+  '<li><a href="/worklogs/webfoundationsworklog.html#wk17" title="Web Foundations Worklog Week 17">Web Foundations<br>Week #17</a></li>' +
+  '<li><a href="/worklogs/webfoundationsworklog.html#wk16" title="Web Foundations Worklog Week 16">Web Foundations<br>Week #16</a></li>' +
+  '<li><a href="/worklogs/webfoundationsworklog.html#wk15" title="Web Foundations Worklog Week 15">Web Foundations<br>Week #15</a></li>' +
+  '</ul>' +
+  '<hr />' +
+  '<h1>Useful Stuff</h1>' +
+  '<hr />' +
+  '<ul>' +
+  '<li><a href="http://www.w3.org/" title="W3C Homepage" rel="external">W3C</a></li>' +
+  '<li><a href="http://validator.w3.org/check?uri=referer" title="W3C HTML Validation Service" rel="external">W3C HTML Validator</a></li>' +
+  '<li><a href="http://jigsaw.w3.org/css-validator/check/referer/" title="W3C CSS Validation Service" rel="external">W3C CSS Validator</a></li>' +
+  '<li><a href="https://www.codecademy.com/" title="Learn to code" rel="external">Code Academy</a></li>' +
+  '<li><a href="http://jisho.org/" title="Japanese-English Dictionary" rel="external">Jisho</a></li>' +
+  '<li><a href="http://www.alljapaneseallthetime.com/blog/" title="All Japanese All The Time - How to learn Japanese in less than 2 years" rel="external">Ajatt</a></li>' +
+  '<li><a href="http://www.antimoon.com/" title="Antimoon - How to learn English effectively" rel="external">Antimoon</a></li>' +
+  '</ul>';
+
+
+document.getElementById("extracontents").innerHTML =
+'<h1>Recent Worklog Entries</h1>' +
+  '<hr />' +
+  '<ul>' +
+  '<li><a href="/worklogs/webfoundationsworklog.html#" title="Web Foundations Worklog Week 17">Web Foundations<br>Week #17</a></li>' +
+  '<li><a href="/worklogs/webfoundationsworklog.html#wk16" title="Web Foundations Worklog Week 16">Web Foundations<br>Week #16</a></li>' +
+  '<li><a href="/worklogs/webfoundationsworklog.html#wk15" title="Web Foundations Worklog Week 15">Web Foundations<br>Week #15</a></li>' +
+  '</ul>' +
+  '<hr />' +
+  '<h1>Useful Stuff</h1>' +
+  '<hr />' +
+  '<ul>' +
+  '<li><a href="http://www.w3.org/" title="W3C Homepage" rel="external">W3C</a></li>' +
+  '<li><a href="http://validator.w3.org/check?uri=referer" title="W3C HTML Validation Service" rel="external">W3C HTML Validator</a></li>' +
+  '<li><a href="http://jigsaw.w3.org/css-validator/check/referer/" title="W3C CSS Validation Service" rel="external">W3C CSS Validator</a></li>' +
+  '<li><a href="https://www.codecademy.com/" title="Learn to code" rel="external">Code Academy</a></li>' +
+  '<li><a href="http://jisho.org/" title="Japanese-English Dictionary" rel="external">Jisho</a></li>' +
+  '<li><a href="http://www.alljapaneseallthetime.com/blog/" title="All Japanese All The Time - How to learn Japanese in less than 2 years" rel="external">Ajatt</a></li>' +
+  '<li><a href="http://www.antimoon.com/" title="Antimoon - How to learn English effectively" rel="external">Antimoon</a></li>' +
+  '</ul>';
