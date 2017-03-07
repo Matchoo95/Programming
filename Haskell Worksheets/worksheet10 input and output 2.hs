cf9512718b41b22e14922fbaedfaf171a31ced3b
Matthew Hawkins <mattmatth123@googlemail.com>
2017-03-07 16:44:36 +0000

    Add files via upload

--- Haskell Worksheets/worksheet10 input and output 2.hs
+++ Haskell Worksheets/worksheet10 input and output 2.hs
@@ -6,9 +6,26 @@ addWord str (x:xs)
     | str == "" = []
     | otherwise = x:xs ++ [str]
     
---2
-wordsToString :: [String] -> String
-wordsToString x = unwords x
+--2 version 1 (works but not correct answer)
+listToString :: [String] -> String
+listToString [] = ""
+listToString x = unwords x
 
---wordsToString1 :: [String] -> String
---wordsToString1 (x:xs) = toString x ++ "\n" wordsToString1 xs
+wordsToString :: [String] -> String
+wordsToString [] = ""
+wordsToString (x:xs) = x ++ "\n" ++ listToString xs
+
+--2 version 2 (with recursion, this is the correct answer)
+wordsToString2 :: [String] -> String
+wordsToString2 [] = ""
+wordsToString2 (x:xs) = x ++ "\n" ++ wordsToString2 xs
+
+--3
+wordsOfLength :: Int -> [String] -> [String]
+wordsOfLength x = filter (\s -> length s == x)
+
+--4 create UI
+
+
+
+
