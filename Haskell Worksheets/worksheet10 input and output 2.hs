dde0e5be56cc0f2c712b3e02ee1493b556c87184
Matthew Hawkins <mattmatth123@googlemail.com>
2017-03-11 16:18:45 +0000

    Add files via upload

--- Haskell Worksheets/worksheet10 input and output 2.hs
+++ Haskell Worksheets/worksheet10 input and output 2.hs
@@ -1,3 +1,46 @@
+helloWorld :: IO ()
+helloWorld = putStrLn "Hello, World!"
+
+displayFile :: IO ()
+displayFile = do 
+    putStr "Enter the filename: "
+    name <- getLine
+    contents <- readFile name
+    putStr contents
+
+getInt :: IO Int
+getInt = do 
+    str <- getLine
+    return (read str :: Int)
+
+isPalindrome :: String -> String
+isPalindrome str
+   | str == reverse str  = str ++ " is a palindrome"
+   | otherwise           = str ++ " is not a palindrome"
+
+pal :: IO ()
+pal = do 
+    line <- getLine
+    let response = isPalindrome line
+    putStrLn response
+
+palLines :: IO ()
+palLines = do 
+    putStr "Enter a line: "
+    str <- getLine
+    if str == "" then 
+        return ()
+    else do 
+        putStrLn (isPalindrome str)
+        palLines
+
+-- For exercise 6
+fahrenheit2Celsius :: Float -> Float
+fahrenheit2Celsius f = (f - 32) * 5 / 9
+ 
+celsius2Fahrenheit :: Float -> Float
+celsius2Fahrenheit c = c * 9 / 5 + 32
+
 --1
 fruit = ["apple", "banana"]
 
@@ -7,25 +50,37 @@ addWord str (x:xs)
     | otherwise = x:xs ++ [str]
     
 --2 version 1 (works but not correct answer)
-listToString :: [String] -> String
-listToString [] = ""
-listToString x = unwords x
+wordsToString1 :: [String] -> String
+wordsToString1 [] = ""
+wordsToString1 x = unwords x
 
-wordsToString :: [String] -> String
-wordsToString [] = ""
-wordsToString (x:xs) = x ++ "\n" ++ listToString xs
-
---2 version 2 (with recursion, this is the correct answer)
 wordsToString2 :: [String] -> String
 wordsToString2 [] = ""
-wordsToString2 (x:xs) = x ++ "\n" ++ wordsToString2 xs
+wordsToString2 (x:xs) = x ++ "\n" ++ wordsToString1 xs
+
+--2 version 2 (with recursion, this is the correct answer)
+wordsToString3 :: [String] -> String
+wordsToString3 [] = ""
+wordsToString3 (x:xs) = x ++ "\n" ++ wordsToString3 xs
 
 --3
 wordsOfLength :: Int -> [String] -> [String]
 wordsOfLength x = filter (\s -> length s == x)
 
 --4 create UI
+main :: IO ()
+main = do
+    contents <- readFile "words.txt"
+    let listWords = read contents :: [String]
+    let newList = addWord "lemon" listWords
+    let listAsString = wordsToString3 newList
+    print listAsString
+    writeFile "words.txt" listAsString
 
 
+    
+--    newList <- actions listWords
+--    let listAsString = show newList
+--    writeFile "words.txt" listAsString
 
 
